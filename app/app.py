d23436df5af258a440c276811a99aa9ef9940619
phillsearle <102220888+phillgames@users.noreply.github.com>
2025-03-19 09:33:35 +0100

    finished the account page, and commented some code

--- app/app.py
+++ app/app.py
@@ -10,5 +10,13 @@ def index():
 def account():
     return render_template('account.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
